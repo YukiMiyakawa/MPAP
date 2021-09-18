7f014525b0d633fc0b7ddac826aab7106297a9dc
YukiMiyakawa <sakomiya3838@gmail.com>
2021-09-18 13:44:29 +0000

    検索ルーティング整理

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,8 @@
 class SearchesController < ApplicationController
   def search
     @range = params[:range]
-    @tag_list= Tag.all
+    @tag_list= Tag.all.limit(6).sort {|a,b| b.post_tags.size <=> a.post_tags.size}
+    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
 
     # 記事タイトルキーワード検索
     if @range == '1'
@@ -18,7 +19,10 @@ class SearchesController < ApplicationController
       negative_keywords.each do |keyword|
         @main_posts.where!("title NOT LIKE ?", "%#{keyword.delete_prefix('-')}%").order(created_at: :DESC)
       end
+      @main_posts = @main_posts.order(created_at: :desc).page(params[:page]).per(9)
       @keywords = params[:keyword]
+      @range = 1
+      render template: "main_posts/index"
 
     # 記事複数タグ検索
     elsif @range == '2'
@@ -28,9 +32,11 @@ class SearchesController < ApplicationController
                                     #1)id列だけ取る    2)指定したタグを元にグルーピング 3)havingはグルーピングしたものに対して条件を書くときに使用する
                                                                                          #distinctは指定したタグをもとにグルーピングを紐解く
       @post_tags = PostTag.where(tag_id: @tags.pluck(:id)).group(:main_post_id).having("count(distinct tag_id)=?",@tags.size)
-      @main_posts = MainPost.find(@post_tags.pluck(:main_post_id))
-
+      @main_posts = MainPost.where(id: @post_tags.pluck(:main_post_id))
+      @main_posts = @main_posts.order(created_at: :desc).page(params[:page]).per(9)
       @keywords = params[:keyword]
+      @range = 2
+      render template: "main_posts/index"
 
     # ユーザー名検索
     elsif @range == '3'
@@ -67,15 +73,25 @@ class SearchesController < ApplicationController
 
   def one_tag_search
     @tag = Tag.find(params[:id])
-    @main_posts = @tag.main_posts
-    @tag_list= Tag.all
+    @main_posts = @tag.main_posts.page(params[:page]).per(9)
+    @tag_list= Tag.all.limit(6).sort {|a,b| b.post_tags.size <=> a.post_tags.size}
+    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
     render template: "main_posts/index"
   end
 
   def index_sort
     selection = params[:sort]
-    @main_posts = MainPost.sort(selection)
-    @tag_list= Tag.all
+    @tag_list= Tag.all.limit(6).sort {|a,b| b.post_tags.size <=> a.post_tags.size}
+    @main_posts = MainPost.all
+
+    if selection == "new" || selection == "old"
+      @main_posts = @main_posts.post_sort(selection).page(params[:page]).per(9)
+    else
+      @main_posts = @main_posts.post_sort(selection)
+      @main_posts = Kaminari.paginate_array(@main_posts).page(params[:page]).per(9)
+    end
+
+    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
     render template: "main_posts/index"
   end
 
@@ -95,9 +111,18 @@ class SearchesController < ApplicationController
     negative_keywords.each do |keyword|
       @main_posts.where!("title NOT LIKE ?", "%#{keyword.delete_prefix('-')}%")
     end
-    @main_posts = @main_posts.post_sort(selection)
+
+    if selection == "new" || selection == "old"
+      @main_posts = @main_posts.post_sort(selection).page(params[:page]).per(9)
+    else
+      @main_posts = @main_posts.post_sort(selection)
+      @main_posts = Kaminari.paginate_array(@main_posts).page(params[:page]).per(9)
+    end
+
     @keywords = params[:keyword]
-    @tag_list= Tag.all
+    @tag_list= Tag.all.limit(6).sort {|a,b| b.post_tags.size <=> a.post_tags.size}
+    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
+    render template: "main_posts/index"
   end
 
   def result_tag_sort
@@ -111,10 +136,19 @@ class SearchesController < ApplicationController
     # byebug
     #<ActiveRecord::Relation SQLを発行してDBをつなげる？　複数レコードを返すような検索を行ったときに帰ってくるインスタンスがwhere
     @main_posts = MainPost.where(id: @post_tags.pluck(:main_post_id))
-    @main_posts = @main_posts.post_sort(selection)
 
+    if selection == "new" || selection == "old"
+      @main_posts = @main_posts.post_sort(selection).page(params[:page]).per(9)
+    else
+      # byebug
+      @main_posts = @main_posts.post_sort(selection)
+      @main_posts = Kaminari.paginate_array(@main_posts).page(params[:page]).per(9)
+    end
     @keywords = params[:keyword]
-    @tag_list= Tag.all
+    @tag_list= Tag.all.limit(6).sort {|a,b| b.post_tags.size <=> a.post_tags.size}
+    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
+    @range = 2
+    render template: "main_posts/index"
   end
 
 end
