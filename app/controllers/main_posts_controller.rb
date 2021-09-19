5706cf854655ea34e6f39829270df4e333e53779
YukiMiyakawa <sakomiya3838@gmail.com>
2021-09-19 07:11:59 +0000

    投稿詳細ページレイアウト

--- app/controllers/main_posts_controller.rb
+++ app/controllers/main_posts_controller.rb
@@ -8,11 +8,33 @@ class MainPostsController < ApplicationController
 
   def show
     @main_post = MainPost.find(params[:id])
+    @user = @main_post.user
+    @tweets = @user.tweets.order(created_at: :desc).page(params[:page]).per(10)
     @sub_post = SubPost.new
     @sub_posts = SubPost.where(main_post_id: @main_post.id)
     @comments = Comment.where(main_post_id: @main_post.id)
     @comment = Comment.new
     @post_tags = @main_post.tags
+
+    @current_entry = Entry.where(user_id: current_user.id)
+    # Entryモデルからメッセージ相手のレコードを抽出
+    @another_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |current|
+        @another_entry.each do |another|
+          # ルームが存在する場合
+          if current.room_id == another.room_id
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      # ルームが存在しない場合は新規作成
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def new
