ebb8b87773cba77da4314d04a1703bb3999e6f6e
YukiMiyakawa <sakomiya3838@gmail.com>
2021-09-26 22:10:28 +0000

    ジャンル、楽器追加メッセージ追加

--- app/controllers/user_music_genres_controller.rb
+++ app/controllers/user_music_genres_controller.rb
@@ -4,11 +4,11 @@ class UserMusicGenresController < ApplicationController
     @user_music_genre = UserMusicGenre.new(user_music_genre_params)
     @user_music_genre.user_id = current_user.id
     if UserMusicGenre.exists?(user_id: @user_music_genre.user_id, music_genre_id: @user_music_genre.music_genre_id)
-      redirect_to request.referer, notice: "追加できませんでした"
+      redirect_to request.referer, flash: { music_genre_error: "既に追加されているジャンルです" }
     elsif @user_music_genre.save
-      redirect_to user_path(current_user)
+      redirect_to user_path(current_user), notice: "ジャンル追加に成功しました"
     else
-      redirect_to request.referer, notice: "追加できませんでした"
+      redirect_to request.referer, flash: { music_genre_error: "ジャンル追加に失敗しました" }
     end
   end
 
@@ -17,7 +17,7 @@ class UserMusicGenresController < ApplicationController
     if user_music_genre.destroy
       redirect_to user_path(current_user)
     else
-      redirect_to request.referer, notice: "削除できませんでした"
+      redirect_to request.referer, flash: { music_genre_error: "ジャンル削除に失敗しました" }
     end
   end
 
