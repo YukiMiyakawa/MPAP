0c9fda0a4e0a9769c29d6fdf13de962bfbf64206
YukiMiyakawa <sakomiya3838@gmail.com>
2021-10-18 13:35:35 +0000

    userモデルテスト作成

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Mpap
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
